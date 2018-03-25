1fc2b7d51c721b956be42b0c9efee14a870f4c62
Miguel Gasca <mgasca@theworkshop.com>
2018-03-25 13:37:51 +0200

    M4 - Platform Group

--- src/index.js
+++ src/index.js
@@ -51,10 +51,22 @@ const GameState = {
       this.ground.body.allowGravity = false;
       this.ground.body.immovable = true;
 
-      this.platform = this.add.sprite(0, 300, 'platform');
-      this.game.physics.arcade.enable(this.platform);
-      this.platform.body.allowGravity = false;
-      this.platform.body.immovable = true;
+      const platformData = [
+          {"x": 0, "y": 430},
+          {"x": 45, "y": 560},
+          {"x": 90, "y": 290},
+          {"x": 0, "y": 140},
+      ];
+
+      this.platforms = this.add.group();
+      this.platforms.enableBody = true;
+
+      platformData.forEach((element) => {
+         this.platforms.create(element.x, element.y, 'platform');
+      }, this);
+
+      this.platforms.setAll('body.immovable', true);
+      this.platforms.setAll('body.allowGravity', false);
 
       //create player
       this.player = this.add.sprite(100, 200, 'player', 3);
@@ -68,7 +80,7 @@ const GameState = {
     },
     update: function() {
         this.game.physics.arcade.collide(this.player, this.ground);
-        this.game.physics.arcade.collide(this.player, this.platform);
+        this.game.physics.arcade.collide(this.player, this.platforms);
 
         this.player.body.velocity.x = 0;
 
