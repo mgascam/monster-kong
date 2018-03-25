35819d1f1c8db2fbb0e8293538f4db9b2de41954
Miguel Gasca <mgasca@theworkshop.com>
2018-03-25 13:26:57 +0200

    M4 - On-screen Controls Part 2

--- src/index.js
+++ src/index.js
@@ -60,7 +60,6 @@ const GameState = {
       this.player = this.add.sprite(100, 200, 'player', 3);
       this.player.anchor.setTo(0.5);
       this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
-      this.player.play('walking');
       this.game.physics.arcade.enable(this.player);
       this.player.customParams = {};
 
